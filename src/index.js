093b6b750f110e16891df40b2b09144d4d4bd449
Avi-Nash-S <nash.dev.22@gmail.com>
2020-11-08 22:58:48 +0530

    🔨Build fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import App from './App';
+import App from './app';
 
 ReactDOM.render(
   <Provider store={store}>
